3d8ef22c4b028f8cdbf684d8040578813bfa48ba
pikri <vickrycepuu@gmail.com>
2026-04-14 10:34:46 +0700

    .

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,15 +1,22 @@
+import { Pool } from "pg"
+import { PrismaPg } from "@prisma/adapter-pg"
 import { PrismaClient } from "@prisma/client"
 
 let prisma: PrismaClient
 
+const connectionString = `${process.env.DATABASE_URL}`
+
+const pool = new Pool({ connectionString })
+const adapter = new PrismaPg(pool)
+
 if (process.env.NODE_ENV === "production") {
-  prisma = new PrismaClient()
+  prisma = new PrismaClient({ adapter })
 } else {
   const globalWithPrisma = global as typeof globalThis & {
     prisma: PrismaClient
   }
   if (!globalWithPrisma.prisma) {
-    globalWithPrisma.prisma = new PrismaClient()
+    globalWithPrisma.prisma = new PrismaClient({ adapter })
   }
   prisma = globalWithPrisma.prisma
 }
